cebfad1f607ff1dbee92f288341f23447f636cdc
Alexandre Mogère <heristop@yahoo.fr>
2012-05-09 19:20:23 +0200

    Init commit

---
+++ src/SenchagridableBehavior.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * @author     Alexandre Mogère
+ * @license    MIT License
+ */
+class SenchagridableBehavior extends Behavior
+{
+    public function queryMethods($builder)
+    {
+        $queryClassName = $builder->getStubQueryBuilder()->getClassname();
+        $objectClassName = $builder->getStubObjectBuilder()->getFullyQualifiedClassname();
+        
+        return "
+/**
+ * Added sort filter on query
+ *
+ * @param Symfony\Component\HttpFoundation\ParameterBag $params
+ * @return $queryClassName
+ */
+public function addGridSortQuery(\$params)
+{
+    if (\$params->get('sort')) {
+        \$sorts = json_decode(\$params->get('sort'));
+        foreach (\$sorts as \$sort) {
+            \$orderBy = \"orderBy\".ucfirst(\$sort->property);
+            \$this->\$orderBy(strtolower(\$sort->direction));
+        }
+    }
+    
+    return \$this;
+}
+
+/**
+ * Added sort filter on query
+ *
+ * @param Symfony\Component\HttpFoundation\ParameterBag $params
+ * @return $queryClassName
+ */
+public function addGridFiltersQuery(\$params)
+{
+    \$i = 0;
+    \$fields = json_decode(\$params->get('fields'));
+    if (!empty(\$fields)) {
+        \$orConds = array();
+        foreach (\$fields as \$field) {
+            \$this->condition(
+                \"cond\$i\",
+                '$objectClassName.'.ucfirst(\$field).' LIKE ?',
+                \"%{\$params->get('query')}%\"
+            );
+            
+            \$orConds[] = \"cond\$i\";
+            \$i++;
+        }
+        
+        \$this->where(\$orConds, 'or');
+    }
+    
+    \$filter = \$params->get('filter');
+    if (!empty(\$filter)) {
+        \$andConds = array();
+        \$count = count(\$filter);
+        for (\$j = 0; \$j < \$count; \$j++) {
+            
+            switch(\$filter[\$j]['data']['type']) {
+                case 'string': {
+                    \$this->condition(
+                        \"cond\$i\",
+                        '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' LIKE ?',
+                        \"%{\$filter[\$j]['data']['value']}%\"
+                    ); break;
+                }
+                case 'list': {
+                    if (strstr(\$filter[\$j]['data']['value'], ',')) {
+                        \$this->condition(
+                            \"cond\$i\",
+                            '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' IN ?',
+                            explode(',', \$filter[\$j]['data']['value'])
+                        );
+                    } else {
+                        \$this->condition(
+                            \"cond\$i\",
+                            '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' = ?',
+                            \$filter[\$j]['data']['value']
+                        );
+                    } break;
+                }
+                case 'boolean': {
+                    \$this->condition(
+                        \"cond\$i\",
+                        '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' = ?',
+                        \"{\$filter[\$j]['data']['value']}\"
+                    ); break;
+                }
+                case 'numeric': {
+                    switch (\$filter[\$j]['data']['comparison']) {
+                        case 'ne': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' != ?',
+                                \"{\$filter[\$j]['data']['value']}\"
+                            ); break;
+                        }
+                        case 'eq': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' = ?',
+                                \"{\$filter[\$j]['data']['value']}\"
+                            ); break;
+                        }
+                        case 'lt': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' < ?',
+                                \"{\$filter[\$j]['data']['value']}\"
+                            ); break;
+                        }
+                        case 'gt': {
+                             \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' > ?',
+                                \"{\$filter[\$j]['data']['value']}\"
+                            ); break;
+                        }
+                    } break;
+                }
+                case 'date': {
+                    switch (\$filter[\$j]['data']['comparison']) {
+                        case 'ne': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' != ?',
+                                \"'\".date('Y-m-d', strtotime(\$filter[\$j]['data']['value'])).\"'\"
+                            ); break;
+                        }
+                        case 'eq': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' = ?',
+                                \"'\".date('Y-m-d', strtotime(\$filter[\$j]['data']['value'])).\"'\"
+                            ); break;
+                        }
+                        case 'lt': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' < ?',
+                                \"'\".date('Y-m-d', strtotime(\$filter[\$j]['data']['value'])).\"'\"
+                            ); break;
+                        }
+                        case 'gt': {
+                            \$this->condition(
+                                \"cond\$i\",
+                                '$objectClassName.'.ucfirst(\$filter[\$j]['field']).' > ?',
+                                \"'\".date('Y-m-d', strtotime(\$filtrer[\$j]['data']['value'])).\"'\"
+                            ); break;
+                        }
+                    }
+                } break;
+            }
+            \$andConds[] = \"cond\$i\";
+            \$i++;
+        }
+        if (!empty(\$andConds)) \$this->where(\$andConds, 'and');
+    }
+    
+    return \$this;
+}
+        ";
+    }
+
+}
